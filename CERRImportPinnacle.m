659e1f189434da1aada2ef8324197eae6f5a55b5
sparkpoints <sparkpoints@gmail.com>
2017-08-09 14:26:21 +0800

    add Pinnacle Version correction
    1, TPS V9.0 above, using x_start_dicom
    2, not import all autoplan create ROIs, like 1FS*, 1PTV*

--- CERRImportPinnacle.m
+++ CERRImportPinnacle.m
@@ -36,8 +36,8 @@ end
 
 %% --------------------------- Get the path of patient directory to be selected for import.
 %pin_path = uigetdir(pwd','Select the directory containing native Pinnacle data');
-pin_path = 'D:\User\Documents\MATLAB\Pinnacledata\Patient_13863';																 
-
+pin_path = 'D:\User\Documents\MATLAB\Pinnacledata\ESO\Patient_9372';																 
+% pin_path = 'D:\User\Documents\MATLAB\Pinnacledata\Patient_Lung';
 if isnumeric(pin_path) || ~exist(fullfile(pin_path,'Patient'),'file')
     disp('Import of Pinnacle data aborted');
     return
@@ -80,15 +80,21 @@ for iScan = 1:length(data_pat.ImageSetList.ImageSet)
         end
         scan_dcm_dir = [ImageName,'.DICOM'];
         planC_w_scan_tmp = readScanFromDir(fullfile(pin_path,scan_dcm_dir));
-        %planC_w_scan_tmp = readScanFromFile(fullfile(pin_path,scan_dcm_dir));
+        %planC_w_scan_tmp =
+        %readScanFromFile(fullfile(pin_path,scan_dcm_dir));
 
         %Populate fields like headInOut etc from Pinnacle file/s
         data_scan = read_1_pinnacle_file(fullfile(pin_path,[ImageName,'.header']),0);
         indexS_tmp = planC_w_scan_tmp{end};
         planC_w_scan_tmp{indexS_tmp.scan}.scanType = data_scan.modality;
         for iSInfo = 1:length(planC_w_scan_tmp{indexS_tmp.scan}.scanInfo)
-            xOffset = data_scan.x_start + planC_w_scan_tmp{indexS_tmp.scan}.scanInfo(iSInfo).grid2Units * (planC_w_scan_tmp{indexS_tmp.scan}.scanInfo(iSInfo).sizeOfDimension2-1)/2;
-            yOffset = data_scan.y_start + planC_w_scan_tmp{indexS_tmp.scan}.scanInfo(iSInfo).grid1Units * (planC_w_scan_tmp{indexS_tmp.scan}.scanInfo(iSInfo).sizeOfDimension1-1)/2;
+            if data_scan.Version >= 9.0 %Pinnacle 9.0 dicom_image dicom begin using _x_start_dicom
+                xOffset = data_scan.x_start_dicom + planC_w_scan_tmp{indexS_tmp.scan}.scanInfo(iSInfo).grid2Units * (planC_w_scan_tmp{indexS_tmp.scan}.scanInfo(iSInfo).sizeOfDimension2-1)/2;
+                yOffset = data_scan.y_start_dicom + planC_w_scan_tmp{indexS_tmp.scan}.scanInfo(iSInfo).grid1Units * (planC_w_scan_tmp{indexS_tmp.scan}.scanInfo(iSInfo).sizeOfDimension1-1)/2;
+            else %Pinnacle 8.X use x_start
+                xOffset = data_scan.x_start + planC_w_scan_tmp{indexS_tmp.scan}.scanInfo(iSInfo).grid2Units * (planC_w_scan_tmp{indexS_tmp.scan}.scanInfo(iSInfo).sizeOfDimension2-1)/2;
+                yOffset = data_scan.y_start + planC_w_scan_tmp{indexS_tmp.scan}.scanInfo(iSInfo).grid1Units * (planC_w_scan_tmp{indexS_tmp.scan}.scanInfo(iSInfo).sizeOfDimension1-1)/2;
+            end
             planC_w_scan_tmp{indexS_tmp.scan}.scanInfo(iSInfo).xOffset = xOffset;
             planC_w_scan_tmp{indexS_tmp.scan}.scanInfo(iSInfo).yOffset = yOffset;
             planC_w_scan_tmp{indexS_tmp.scan}.scanInfo(iSInfo).headInOut = data_scan.patient_position;
@@ -134,28 +140,102 @@ else
 end
 
 %
+plan_info = {};
 for iPlan = 1:length(data_pat.PlanList.Plan)
     if length(data_pat.PlanList.Plan) == 1
         imageSetID = data_pat.PlanList.Plan.PrimaryCTImageSetID;
+        planID = data_pat.PlanList.Plan.PlanID;
+        plan_name  = data_pat.PlanList.Plan.PlanName;
+        plan_dosimetrist = data_pat.PlanList.Plan.Dosimetrist;
+        plan_physicist = data_pat.PlanList.Plan.Physicist;
+        plan_comment = data_pat.PlanList.Plan.Comment;
+        plan_TPS_version = data_pat.PlanList.Plan.PinnacleVersionDescription;
+        plan_lock = data_pat.PlanList.Plan.PlanIsLocked;
     else
         imageSetID = data_pat.PlanList.Plan{iPlan}.PrimaryCTImageSetID;
+        planID = data_pat.PlanList.Plan{iPlan}.PlanID;
+        plan_name  = data_pat.PlanList.Plan{iPlan}.PlanName;
+        plan_dosimetrist = data_pat.PlanList.Plan{iPlan}.Dosimetrist;
+        plan_physicist = data_pat.PlanList.Plan{iPlan}.Physicist;
+        plan_comment = data_pat.PlanList.Plan{iPlan}.Comment;
+        plan_TPS_version = data_pat.PlanList.Plan{iPlan}.PinnacleVersionDescription;
+        plan_lock = data_pat.PlanList.Plan{iPlan}.PlanIsLocked;
+    end
+     
+    if (strfind(upper(plan_name),'QA') > 0)
+        %data_pat.PlanList.Plan{iPlan} = {};
+        continue;
+    end
+    
+    if strfind(upper(plan_name),'COPY') %searching duplicate copy plan of previous plan, skip importing
+        duplicate_plan = 0;
+        for  indexPlan = 1:length(plan_list)
+            if isempty(plan_list{indexPlan})
+                continue;
+            else
+                ind_plan = plan_list{indexPlan}.index;
+                if (imageSetID == plan_list{ind_plan}.imageSetID) && strcmp(plan_comment,plan_list{ind_plan}.plan_comment)
+                    duplicate_plan = 1;
+                end
+            end
+        end
+        if duplicate_plan
+            %data_pat.PlanList.Plan{iPlan} = {};
+            continue;
+        end
+    end
+    
+    % if plan is not locked (plan_lock = 0), the plan parameter and plan
+    % not trustable
+    if null(plan_lock)
+        continue;
+    end
+    %the QA plans do not contain CT images in the local plan dir, not parsing
+    if (imageSetID == -1) 
+        continue;
     end
     assocScanNum = find(ImageSetIDv == imageSetID);
     if length(planC{indexS.scan}) < assocScanNum
         continue;
+    end   
+    
+    if isdir(fullfile(pin_path,['Plan_',num2str(planID)]))
+        plan_list{iPlan}.index = iPlan;
+        plan_list{iPlan}.imageSetID = imageSetID;
+        plan_list{iPlan}.planID = planID;
+        plan_list{iPlan}.plan_name = plan_name;
+        plan_list{iPlan}.plan_dosimetrist = plan_dosimetrist;
+        plan_list{iPlan}.plan_physicist = plan_physicist;
+        plan_list{iPlan}.plan_comment = plan_comment;
+        plan_list{iPlan}.plan_TPS_version = plan_TPS_version;
+        plan_list{iPlan}.plan_lock = plan_lock;    
     end
+    
     zValuesV = [planC{indexS.scan}(assocScanNum).scanInfo(:).zValue];
-    if length(data_pat.PlanList.Plan) == 1
-        planID = data_pat.PlanList.Plan.PlanID;
-    else
-        planID = data_pat.PlanList.Plan{iPlan}.PlanID;
-    end
+    
+    %read structures from ROI files
     roi_filename = fullfile(pin_path,['Plan_',num2str(planID)],'plan.roi');
     data_plan_roi = read_1_pinnacle_file(roi_filename,0);
     if isfield(data_plan_roi,'roi') && ~isempty(data_plan_roi)
         for iStruct = 1:length(data_plan_roi.roi)
+            %remove some auxilary contours (auto-create by some scripts)
+            if ~isempty(regexp(upper(data_plan_roi.roi{iStruct}.name),'^1FS*', 'once')) 
+                continue;
+            end
+            if ~isempty(regexp(upper(data_plan_roi.roi{iStruct}.name),'^1PTV*', 'once'))
+                continue;
+            end
+            if ~isempty(regexp(upper(data_plan_roi.roi{iStruct}.name),'^PATIENT*', 'once'))
+                continue;
+            end
+            if ~isempty(regexp(upper(data_plan_roi.roi{iStruct}.name),'^FS*', 'once'))
+                continue;
+            end
             planC{indexS.structures}(end+1) = newCERRStructure(assocScanNum,planC);
             planC{indexS.structures}(end).structureName = num2str(data_plan_roi.roi{iStruct}.name);
+            planC{indexS.structures}(end).studyNumberOfOrigin = planID; %add planID mark to ROIs
+            planC{indexS.structures}(end).roiNumber  = data_plan_roi.roi{iStruct}.num_curve; %contour numbers of current Roi
+%             planC{indexS.structures}(end).structureDescription = data_plan_roi.roi{iStruct}.volume; %contour volume of current ROI
             if isfield(data_plan_roi.roi{iStruct},'curve')
                 for iSlice = 1:length(data_plan_roi.roi{iStruct}.curve)
                     if iscell(data_plan_roi.roi{iStruct}.curve)
@@ -231,11 +311,14 @@ planC = setUniformizedData(planC);
 %% ---------------------------  Populate indexS.dose field
 
 for iPlan = 1:length(data_pat.PlanList.Plan)
-    fprintf('Plan_name: %s\n',data_pat.PlanList.Plan{iPlan,1}.PlanName);
+   
     if length(data_pat.PlanList.Plan) == 1
         imageSetID = data_pat.PlanList.Plan.PrimaryCTImageSetID;
     else
         imageSetID = data_pat.PlanList.Plan{iPlan}.PrimaryCTImageSetID;
+    end
+    if (imageSetID == -1)
+        continue;
     end
     assocScanNum = find(ImageSetIDv == imageSetID);
 
@@ -244,7 +327,7 @@ for iPlan = 1:length(data_pat.PlanList.Plan)
     else
         planID = data_pat.PlanList.Plan{iPlan}.PlanID;
     end
-
+    fprintf('Plan_name: plan_%d\n',planID);
     planPath = fullfile(pin_path,['Plan_',num2str(planID)]);
 
     if exist(fullfile(planPath,'plan.Trial'),'file')
@@ -280,6 +363,7 @@ for iPlan = 1:length(data_pat.PlanList.Plan)
         end
 
         trialName = Trial.Name;
+        fprintf('Trial_name: %s\n',trialName);
 
         plan = Trial;
         %         plan = read_pinnacle_plan_trial(fullfile(pin_path,['Plan_',num2str(planID)],'plan.Trial'),trialName);
@@ -418,6 +502,30 @@ for iPlan = 1:length(data_pat.PlanList.Plan)
         else
             planC{indexS.dose}(end).doseArray = planC{indexS.dose}(end).doseArray;
         end
+        
+        %calculate parameters
+        for structNum = 1: length(planC{indexS.structures}) 
+            if isempty(planC{indexS.structures}(1,structNum).roiNumber) %empty ROIs (with zero curves)
+                continue;
+            end
+            if (planC{indexS.structures}(1,structNum).studyNumberOfOrigin ~= planID) %mul-plans, skip contours which not belong to this plan
+                continue;
+            end
+            doseBinsV = [];
+            volsHistV = [];
+            [planC,doseBinsV,volsHistV] = getDVHMatrix(planC,structNum,length(planC{indexS.dose}));
+            structVolume = calc_abs_vol(doseBinsV,volsHistV);
+            maxdose = round(calc_maxDose(doseBinsV,volsHistV,2))*0.01;
+            meandose = round(calc_meanDose(doseBinsV,volsHistV,2))*0.01;
+            mindose = round(calc_minDose(doseBinsV,volsHistV,2))*0.01;
+            fprintf('%d:%s,volume=%6.2f,maxdose=%6.2f,Meandose=%6.2f,mindose=%6.2f\n',structNum,planC{indexS.structures}(structNum).structureName,structVolume,maxdose,meandose,mindose);
+            if strcmpi(planC{indexS.structures}(1,structNum).structureName,'LUNG_TOTAL')
+                V5 = calc_Vx(doseBinsV, volsHistV, 500, 2)/structVolume;
+                V20  = calc_Vx(doseBinsV, volsHistV, 2000, 2)/structVolume;
+                V30  = calc_Vx(doseBinsV, volsHistV, 3000, 2)/structVolume;
+                fprintf('Lung_toatal:V5=%3.2f,V20=%3.2f,V30=%3.2f\n',V5,V20,V30);
+            end
+        end
 
     end %trial
 
@@ -426,12 +534,15 @@ end %plan
 %% ---------------------------  Populate indexS.beams field
 
 toc;
-indexS = planC{end};
 
-%% --------------------------- Save planC
-%save_planC(planC,planC{indexS.CERROptions});
+ 
+% %% ---------------------------- Print Out ROIs' Statistical Info 
+% indexS = planC{end};
 
-%% export to dicom-rt
+% %% --------------------------- Save planC
+% %save_planC(planC,planC{indexS.CERROptions});
+% 
+% %%--------------------------------- export to dicom-rt
 destDir = 'D:\User\Documents\MATLAB\bb';
 export_planC_to_DICOM(planC,destDir);
 
---
+++ PointsDose.m
@@ -0,0 +1,4 @@
+x = data_plan_points.Poi{2,1}.XCoord
+y = data_plan_points.Poi{2,1}.YCoord
+z = data_plan_points.Poi{2,1}.ZCoord
+dotse = getDoseAt(1,x,y,z,planC)
